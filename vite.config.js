8e98b26d1050a640b7d522ee26a90ba21a39771f
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-12-06 11:53:41 +0530

    update vite configration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 // import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(
+    {
+      template: {
+        transformAssetUrls: {
+            includeAbsolute: false,
+        },
+    },
+    }
+  )],
 });
