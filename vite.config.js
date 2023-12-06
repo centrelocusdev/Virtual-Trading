48527f2d191f2bb507a0d550ac7181d2f2614988
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-12-06 12:03:02 +0530

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -8,21 +8,21 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
+        chunkFileNames: '/[name]-[hash].js',
+        entryFileNames: '/[name]-[hash].js',
         
         assetFileNames: ({name}) => {
           if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
-              return 'assets/images/[name]-[hash][extname]';
+              return '/[name]-[hash][extname]';
           }
           
           if (/\.css$/.test(name ?? '')) {
-              return 'assets/css/[name]-[hash][extname]';   
+              return '/[name]-[hash][extname]';   
           }
  
           // default value
           // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-          return 'assets/[name]-[hash][extname]';
+          return '/[name]-[hash][extname]';
         },
       },
     }
